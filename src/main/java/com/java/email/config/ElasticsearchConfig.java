6748b39a2ad6ddc8118b5cb1129672c8dec7f887
LiuSir887 <13149511+liusir887@user.noreply.gitee.com>
2025-01-20 15:03:53 +0800

    修改了pom文件，es的config

--- src/main/java/com/java/email/config/ElasticsearchConfig.java
+++ src/main/java/com/java/email/config/ElasticsearchConfig.java
@@ -6,29 +6,28 @@ import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
+@EnableElasticsearchRepositories(basePackages = "com.java.email.esdao.repository")
 public class ElasticsearchConfig {
 
-    private static final String HOST = "localhost";
-    private static final int PORT = 9200;
-    private static final String SCHEME = "http";
+    @Value("${spring.elasticsearch.uris}")
+    private String elasticsearchUrl;
 
     @Bean
     public RestClient restClient() {
-        return RestClient.builder(
-            new HttpHost(HOST, PORT, SCHEME)
-        ).build();
+        String host = elasticsearchUrl.replace("http://", "").split(":")[0];
+        int port = Integer.parseInt(elasticsearchUrl.replace("http://", "").split(":")[1]);
+        return RestClient.builder(new HttpHost(host, port)).build();
     }
 
     @Bean
     public ElasticsearchTransport elasticsearchTransport(RestClient restClient) {
-        return new RestClientTransport(
-            restClient,
-            new JacksonJsonpMapper()
-        );
+        return new RestClientTransport(restClient, new JacksonJsonpMapper());
     }
 
     @Bean
