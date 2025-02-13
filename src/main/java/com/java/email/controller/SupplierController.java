522f3f1e3faa95ba7d79d9542f0d37b7a5da134c
彭于晏 <3435185298@qq.com>
2025-02-13 21:51:10 +0800

    1

--- src/main/java/com/java/email/controller/SupplierController.java
+++ src/main/java/com/java/email/controller/SupplierController.java
@@ -8,7 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/supplier")
+@RequestMapping("/suppliers")
 public class SupplierController {
     @Autowired
     private SupplierService supplierService;
