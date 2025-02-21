b4c5079872dee4f0e829d092181e0475ec2de0b9
tangxinkang <2912528586@qq.com>
2025-02-22 02:46:27 +0800

    添加了拦截器，拦截请求验证token

--- src/main/java/com/java/email/config/WebConfig.java
+++ src/main/java/com/java/email/config/WebConfig.java
@@ -1,18 +1,42 @@
 package com.java.email.config;
 
+import com.java.email.aop.interceptors.CheckTokenInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.client.RestTemplate;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private CheckTokenInterceptor checkTokenInterceptor;
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .maxAge(3600);
+    public void addInterceptors(InterceptorRegistry registry) {
+        List<String> excludePathList=new ArrayList<>();
+        excludePathList.add("/user/login");
+        excludePathList.add("/user/logout");
+        excludePathList.add("/error");
+        registry.addInterceptor(checkTokenInterceptor).addPathPatterns("/**").excludePathPatterns(excludePathList);
     }
-} 
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(5000);
+        requestFactory.setReadTimeout(5000);
+
+        restTemplate.setRequestFactory(requestFactory);
+
+        return restTemplate;
+    }
+}
