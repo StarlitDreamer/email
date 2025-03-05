27dc88177b7aa705d0ffb883c9bb331401d98151
tangxinkang <2912528586@qq.com>
2025-03-05 15:51:32 +0800

    给es配置加上了用户名、密码

--- src/main/java/com/java/email/config/ElasticsearchConfig.java
+++ src/main/java/com/java/email/config/ElasticsearchConfig.java
@@ -5,6 +5,9 @@ import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,11 +21,29 @@ public class ElasticsearchConfig {
     @Value("${spring.elasticsearch.uris}")
     private String elasticsearchUrl;
     
+    @Value("${spring.elasticsearch.username}")
+    private String username;
+    
+    @Value("${spring.elasticsearch.password}")
+    private String password;
+    
     @Bean
     public RestClient restClient() {
         String host = elasticsearchUrl.replace("http://", "").split(":")[0];
         int port = Integer.parseInt(elasticsearchUrl.replace("http://", "").split(":")[1]);
-        return RestClient.builder(new HttpHost(host, port)).build();
+        
+        // 创建认证提供者
+        final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(
+            AuthScope.ANY,
+            new UsernamePasswordCredentials(username, password)
+        );
+        
+        // 构建 RestClient，添加认证信息
+        return RestClient.builder(new HttpHost(host, port))
+            .setHttpClientConfigCallback(httpClientBuilder -> 
+                httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
+            .build();
     }
 
     @Bean
